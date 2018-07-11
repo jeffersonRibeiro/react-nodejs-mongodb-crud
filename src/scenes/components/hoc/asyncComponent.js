c2a06b7ac2a77b8c3b1e5e9fa5183223137caffe
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-07-11 12:19:37 -0300

    Update asyncComponent.js

--- src/scenes/components/hoc/asyncComponent.js
+++ src/scenes/components/hoc/asyncComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const asyncComponent = (importComponent) => {
+const asyncComponent = importComponent => {
   return class extends Component {
     state = {
       component: null,
